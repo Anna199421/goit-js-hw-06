0409638c639504a4e1b0306ac9f3c6091867dfd0
Anna199421 <111239829+Anna199421@users.noreply.github.com>
2023-01-15 13:21:05 +0200

    Update index.html

---
+++ js/task-04.js
@@ -0,0 +1,12 @@
+const decrem = document.querySelector("#counter button[data-action = decrement]")
+const increm = document.querySelector("#counter button[data-action = increment]")
+const span = document.querySelector("#value")
+let counterValue = 0
+const dec = () => {
+    span.textContent = counterValue -=1
+}
+const inc = () => {
+    span.textContent = counterValue +=1
+}
+decrem.addEventListener("click", dec)
+increm.addEventListener("click", inc)
